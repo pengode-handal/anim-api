2eda92680b5aace9f669578a44f47ff4ad51c20e
Saip <saifpatih@gmail.com>
2022-04-22 17:43:48 +0700

    apdet lagi

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ def api():
     }
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=int(os.environ.get('PORT','80')),debug=True)
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT','5000')),debug=True)
